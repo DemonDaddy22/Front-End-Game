524fdeb1d6d515069fe5b1cd13f709646cf1cb08
Rohan Gupta <rohan.gupta@gameskraft.com>
2022-01-05 00:10:05 +0530

    Create cell component and render sudoku board

--- src/Sudoku/DemonDaddy22/ReactJS/index.tsx
+++ src/Sudoku/DemonDaddy22/ReactJS/index.tsx
@@ -9,13 +9,21 @@
 // TODO - for resetting, take some other puzzle and create a new board using it
 
 import React, { useCallback, useEffect, useState } from 'react';
+import { createListOfSize } from '../../../utils';
 import puzzles from '../sudoku.json';
+import Cell from './components/Cell';
+import classes from './styles.module.scss';
 
 const Sudoku: React.FC<{}> = () => {
     const [puzzle, setPuzzle] = useState<Sudoku | null>(null);
     const [sudokuPuzzles, setSudokuPuzzles] = useState<Array<Sudoku> | null>(
         null
     );
+    const [originalMoves, setOriginalMoves] = useState<Array<Move>>([]);
+    const [manipulatedMoves, setManipulatedMoves] = useState<Array<Move>>([]);
+
+    const size = Math.floor(Math.sqrt(puzzle?.quizzes?.length || 0));
+    const puzzleRow = createListOfSize(size);
 
     const setSudokuPuzzle = useCallback(() => {
         if (sudokuPuzzles?.length) {
@@ -42,7 +50,54 @@ const Sudoku: React.FC<{}> = () => {
         setSudokuPuzzle();
     }, [setSudokuPuzzle]);
 
-    return <></>;
+    const handleValueChange = useCallback(
+        (index: number, value: number | string) => {
+            setPuzzle((prevPuzzleSnap) => {
+                let newPuzzleSnap = prevPuzzleSnap;
+                if (prevPuzzleSnap) {
+                    newPuzzleSnap = {
+                        quizzes:
+                            prevPuzzleSnap.quizzes?.slice(0, index) +
+                            value +
+                            prevPuzzleSnap.quizzes?.slice(index + 1),
+                        solutions: prevPuzzleSnap.solutions,
+                    };
+                }
+                return newPuzzleSnap;
+            });
+            setOriginalMoves((prevMoves) => [...prevMoves, { index, value }]);
+            setManipulatedMoves((prevMoves) => [
+                ...prevMoves,
+                { index, value },
+            ]);
+        },
+        []
+    );
+
+    return puzzle ? (
+        <div className={classes.puzzleContainer}>
+            {puzzleRow.map((_, rowIndex) => {
+                return (
+                    <div key={`row-${rowIndex}`} className={classes.puzzleRow}>
+                        {puzzleRow.map((cell, columnIndex) => {
+                            return (
+                                <Cell
+                                    key={`cell-${rowIndex}-${columnIndex}`}
+                                    data={
+                                        puzzle?.quizzes?.[
+                                            rowIndex * size + columnIndex
+                                        ]
+                                    }
+                                    index={rowIndex * size + columnIndex}
+                                    onChange={handleValueChange}
+                                />
+                            );
+                        })}
+                    </div>
+                );
+            })}
+        </div>
+    ) : null;
 };
 
 export default Sudoku;
--- src/types/sudoku.d.ts
+++ src/types/sudoku.d.ts
@@ -2,3 +2,14 @@ interface Sudoku {
     quizzes: string;
     solutions: string;
 }
+
+interface Cell {
+    index: number;
+    data: number | string;
+    onChange: (...args: any[]) => any;
+}
+
+interface Move {
+    index: number;
+    value: number | string;
+}
