782c912a87e2faa0edf0a513cc817454be515aca
Robin Singla <36002305+singlarobin@users.noreply.github.com>
2022-01-15 13:54:24 +0530

    Singlarobin/star rating (#14)
    * Restyle Sudoku/singlarobin/react (#10)
    
    * components created for game
    
    * loader component created and api call is made to get sudoku data
    
    * local storage is added to maintain state in the browser
    
    * updating input value is synced with local storage
    
    * button features functionality implemented and removed unused code
    
    * Restyled by clang-format
    
    * Restyled by prettier
    
    * Restyled by whitespace
    
    Co-authored-by: singlarobin <singlarobin99@gmail.com>
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    * sudoku folder deleted
    
    * star ratings project created
    
    * star rating project template copied
    
    * star rating functionality implemented
    
    * read me file created
    
    * Update README.md
    
    UI gif is added
    
    * event delegation is added
    
    * Restyled by clang-format
    
    * Restyled by prettier
    
    * Restyled by prettier-markdown
    
    * Restyled by whitespace
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

---
+++ src/StarRatings/singlarobin/VanillaJS/index.js
@@ -0,0 +1,59 @@
+/*
+ * Creates star rating functionality
+ * @param el DOM Element
+ * @param count Number of stars
+ * @param callback Returns selected star count to callback
+ */
+let lastClickedId = 0;
+
+function Star(el, count, callback) {
+    // write logic to create star rating utility.
+    const starWrapper = document.querySelector(el);
+    for (let i = 1; i <= count; i++) {
+        const element = document.createElement('i');
+        element.setAttribute('id', i);
+        element.classList.add('fa', 'fa-star-o');
+        starWrapper.appendChild(element);
+    }
+    starWrapper.addEventListener('click', (e) =>
+        handleClick(e, count, callback)
+    );
+    starWrapper.addEventListener('mouseover', handleHoverEnter);
+    starWrapper.addEventListener('mouseout', handleHoverOut);
+}
+
+function handleHoverEnter(e) {
+    const id = e.target.id;
+    for (let i = lastClickedId === 0 ? 1 : lastClickedId; i <= id; i++) {
+        const starElement = document.getElementById(i);
+        starElement.classList.add('fa-star');
+        starElement.classList.remove('fa-star-o');
+    }
+}
+
+function handleHoverOut(e) {
+    const id = e.target.id;
+    if (parseInt(id) > lastClickedId) {
+        for (let i = lastClickedId + 1; i <= parseInt(id); i++) {
+            const starElement = document.getElementById(i);
+            starElement.classList.remove('fa-star');
+            starElement.classList.add('fa-star-o');
+        }
+    }
+}
+
+function handleClick(e, count, callback) {
+    const id = e.target.id;
+    for (let i = 1; i <= id; i++) {
+        const starElement = document.getElementById(i);
+        starElement.classList.add('fa-star');
+        starElement.classList.remove('fa-star-o');
+    }
+    for (let i = parseInt(id) + 1; i <= count; i++) {
+        const starElement = document.getElementById(i);
+        starElement.classList.remove('fa-star');
+        starElement.classList.add('fa-star-o');
+    }
+    lastClickedId = parseInt(id);
+    callback(id);
+}
