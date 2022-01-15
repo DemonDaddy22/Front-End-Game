8b11020c6fe4affd3a3f630e47d700e56835a140
singlarobin <singlarobin99@gmail.com>
2022-01-15 19:45:22 +0530

    Pixel Art Project template created

---
+++ src/PixelArt/Singlarobin/VanillaJS/index.js
@@ -0,0 +1,9 @@
+/*
+ * Creates pixel art grid
+ * @param el DOM Element
+ * @param rows Number of rows
+ * @param rows Number of cols
+ */
+function PixelArt(el, rows, cols) {
+    // write logic to create pixel art grid.
+}
