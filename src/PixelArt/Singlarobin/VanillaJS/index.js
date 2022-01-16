f94067498114ed8744401512dfde8d3cf45a5a0c
singlarobin <singlarobin99@gmail.com>
2022-01-16 18:03:01 +0530

    drag event is corrected and made sure that color string is of length 6

--- src/PixelArt/Singlarobin/VanillaJS/index.js
+++ src/PixelArt/Singlarobin/VanillaJS/index.js
@@ -5,6 +5,8 @@
  * @param rows Number of cols
  */
 let selectedColor = '';
+let draggable = false;
+
 function PixelArt(el, rows, cols) {
     // write logic to create pixel art grid.
     const cellWidthString = getComputedStyle(
@@ -26,14 +28,13 @@ function PixelArt(el, rows, cols) {
     for (let i = 0; i <= rows; i++) {
         for (let j = 0; j < cols; j++) {
             const element = document.createElement('div');
-            element.setAttribute('id', `${i} ${j} `);
+            element.id = `${i} ${j} `;
+            element.dataset.type = 'cell';
             element.classList.add('cell');
             gridElement.appendChild(element);
             if (i === rows) {
                 element.classList.add('color');
                 element.style.background = color[j];
-            } else {
-                element.setAttribute('draggable', true);
             }
         }
     }
@@ -45,12 +46,19 @@ function PixelArt(el, rows, cols) {
     gridElement.appendChild(bottomBorder);
 
     gridElement.addEventListener('click', (e) => handleClick(e, rows, color));
-    gridElement.addEventListener('dragover', (e) => handleDrag(e, rows));
+    gridElement.addEventListener('mousedown', handleMouseEvent);
+    gridElement.addEventListener('mouseover', (e) => handleDrag(e, rows));
+    gridElement.addEventListener('mouseup', handleMouseEvent);
 }
 
 function getColorValue(list) {
     let color = Math.floor(Math.random() * 16777215).toString(16);
-    while (color === '011d49' || color === '808080' || list.includes(color)) {
+    while (
+        color.length !== 6 ||
+        color === '011d49' ||
+        color === '808080' ||
+        list.includes(color)
+    ) {
         color = Math.floor(Math.random() * 16777215).toString(16)
     }
     return color;
@@ -58,8 +66,11 @@ function getColorValue(list) {
 
 function handleClick(e, lastRow, color) {
     const id = e.target.id;
+    const type = e.target.dataset.type;
+    if (type !== 'cell') return;
     const [row, col] = id.split(' ');
     const element = document.getElementById(id);
+    // console.log(id);
     if (parseInt(row) === lastRow) {
         selectedColor = color[col];
         return;
@@ -67,10 +78,15 @@ function handleClick(e, lastRow, color) {
     element.style.background = selectedColor;
 }
 
+function handleMouseEvent() {
+    draggable = !draggable;
+}
+
 function handleDrag(e, lastRow) {
     const id = e.target.id;
     const element = document.getElementById(id);
     const row = id.split(' ')[0];
+    if (!draggable) return;
     if (parseInt(row) === lastRow) {
         return;
     }
