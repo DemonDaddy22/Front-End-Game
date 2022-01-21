a1c2401bbaf8d7d4ba8cf30a9a2ec257dda41102
singlarobin <singlarobin99@gmail.com>
2022-01-21 22:26:52 +0530

    Implemented progress bar exercise

---
+++ src/ProgressBar/SinglaRobin/README.md
@@ -0,0 +1,6 @@
+## Progress Bar
+
+## By [SinglaRobin](https://github.com/singlarobin)
+
+-   Implemented the solution in 90 minutes in Vanilla JS.
+-   Was stuck in making queue for multiple progress.
---
+++ src/ProgressBar/SinglaRobin/VanillaJS/index.js
@@ -0,0 +1,42 @@
+(function () {
+    const barElement = document.getElementById('progress-bar');
+    const runButton = document.getElementById('run-button');
+    const spanElement = document.querySelector('#run-button span');
+    //Time is in seconds
+    const animationTime = 3;
+    const maxProgress = 100;
+    let progress = 0;
+    let run = 0;
+    let timerId = null;
+
+    const updateBarValue = () => {
+        if (progress >= 100) {
+            progress = 0;
+            barElement.value = 0;
+            clearInterval(timerId);
+            timerId = null;
+            run--;
+            if (run === 0) {
+                spanElement.innerHTML = '';
+            } else {
+                spanElement.innerHTML = run;
+                handleProgressBar();
+            }
+        } else {
+            progress += maxProgress / animationTime;
+            barElement.value = progress;
+        }
+    };
+
+    const handleProgressBar = () => {
+        timerId = setInterval(() => updateBarValue(), 1000);
+    };
+
+    const handleButtonClick = () => {
+        run++;
+        spanElement.innerHTML = run;
+        if (!timerId) handleProgressBar();
+    };
+
+    runButton.addEventListener('click', handleButtonClick);
+})();
