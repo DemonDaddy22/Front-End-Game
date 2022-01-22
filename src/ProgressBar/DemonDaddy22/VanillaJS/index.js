c1961ee51dad009834338d46c8e10737f841b1d9
Restyled.io <commits@restyled.io>
2022-01-22 20:45:11 +0000

    Restyled by prettier

--- src/ProgressBar/DemonDaddy22/VanillaJS/index.js
+++ src/ProgressBar/DemonDaddy22/VanillaJS/index.js
@@ -1,47 +1,49 @@
-(function() {
-const progressBar = document.querySelector('.progress-bar');
-const runButton = document.querySelector('#run');
+(function () {
+    const progressBar = document.querySelector('.progress-bar');
+    const runButton = document.querySelector('#run');
 
-const animationDuration = 1500;
-let playCount = 0;
-let isPlaying = false;
+    const animationDuration = 1500;
+    let playCount = 0;
+    let isPlaying = false;
 
-progressBar.style.animationDuration = `${animationDuration}ms`;
+    progressBar.style.animationDuration = `${animationDuration}ms`;
 
-const handleBarAnimation = () => {
-  isPlaying = true;
-  // use animate interface of element to fill the bar
-  // on animation finish, check if count is non-zero
-  // then re-call the animation function and update the button content
-  // else, set isPlaying to false and update the button content
-  progressBar
-      .animate({flexBasis : [ 0, '100%' ]}, {
-        duration : animationDuration,
-        easing : 'linear',
-        iterations : 1,
-      })
-      .finished.then(() => {
-        playCount--;
-        if (playCount > 0) {
-          runButton.textContent = `Run ${playCount}`;
-          handleBarAnimation();
-        } else {
-          isPlaying = false;
-          runButton.textContent = 'Run';
-        }
-      });
-};
+    const handleBarAnimation = () => {
+        isPlaying = true;
+        // use animate interface of element to fill the bar
+        // on animation finish, check if count is non-zero
+        // then re-call the animation function and update the button content
+        // else, set isPlaying to false and update the button content
+        progressBar
+            .animate(
+                { flexBasis: [0, '100%'] },
+                {
+                    duration: animationDuration,
+                    easing: 'linear',
+                    iterations: 1,
+                }
+            )
+            .finished.then(() => {
+                playCount--;
+                if (playCount > 0) {
+                    runButton.textContent = `Run ${playCount}`;
+                    handleBarAnimation();
+                } else {
+                    isPlaying = false;
+                    runButton.textContent = 'Run';
+                }
+            });
+    };
 
-// update count on button click
-// queue animation calls on each click
-const handleRunButtonClick = () => {
-  playCount++;
-  runButton.textContent = `Run ${playCount}`;
+    // update count on button click
+    // queue animation calls on each click
+    const handleRunButtonClick = () => {
+        playCount++;
+        runButton.textContent = `Run ${playCount}`;
 
-  if (!isPlaying)
-    handleBarAnimation();
-};
+        if (!isPlaying) handleBarAnimation();
+    };
 
-// click event listener
-runButton.addEventListener('click', handleRunButtonClick);
+    // click event listener
+    runButton.addEventListener('click', handleRunButtonClick);
 })();
