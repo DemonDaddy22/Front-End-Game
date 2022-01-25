31758c001f001ecd701d92e2c6121cadfafcd0a4
Rohan Gupta <rohan.gupta@gameskraft.com>
2022-01-26 00:16:58 +0530

    Add game logic

--- src/MemoryGame/DemonDaddy22/VanillaJS/index.js
+++ src/MemoryGame/DemonDaddy22/VanillaJS/index.js
@@ -6,6 +6,11 @@
 
     let cScore = 0;
     let hScore = JSON.parse(localStorage.getItem('memoryGameHighScore')) || 0;
+    highScore.textContent = hScore;
+
+    let blocksSequence = [];
+    let gameCounter = 0;
+    let showingSequence = false;
 
     // create blocks container
     (function () {
@@ -21,13 +26,68 @@
 
     const generateMoveSequence = () => {
         const sequence = [];
-        for (let i = 0; i < cScore; i++) {
+        for (let i = 0; i < cScore + 1; i++) {
             sequence.push(Math.floor(Math.random() * size));
         }
         return sequence;
     };
 
-    const handleStartGame = () => {};
+    const handleStartGame = () => {
+        startButton.disabled = true;
+        showingSequence = true;
+        gameCounter = 0;
+        blocksSequence = generateMoveSequence();
 
-    const handleBlockClick = (e) => {};
+        blocksSequence.forEach((blockId, index) => {
+            const block = blocks[blockId];
+            setTimeout(() => {
+                block.classList.add('background-correct');
+            }, 500 + 1000 * index);
+            setTimeout(() => {
+                block.classList.remove('background-correct');
+                if (index === blocksSequence.length - 1) {
+                    showingSequence = false;
+                }
+            }, 1000 * (index + 1));
+        });
+    };
+
+    const handleBlockClick = (e) => {
+        if (showingSequence || !blocksSequence.length) return;
+
+        const block = e.target;
+        if (!block || !block.classList.contains('block')) return;
+
+        if (block.dataset.id == blocksSequence[gameCounter]) {
+            gameCounter++;
+            block.classList.add('background-correct');
+            setTimeout(() => block.classList.remove('background-correct'), 250);
+
+            if (gameCounter === blocksSequence.length) {
+                startButton.disabled = false;
+                cScore = cScore + 1;
+                hScore = Math.max(hScore, cScore);
+                currentScore.textContent = cScore;
+                highScore.textContent = hScore;
+                blocksSequence = [];
+                localStorage.setItem(
+                    'memoryGameHighScore',
+                    JSON.stringify(hScore)
+                );
+            }
+        } else {
+            block.classList.add('background-incorrect');
+            blocksContainer.classList.add('shake');
+            cScore = 0;
+            blocksSequence = [];
+            currentScore.textContent = cScore;
+            setTimeout(() => {
+                block.classList.remove('background-incorrect');
+                blocksContainer.classList.remove('shake');
+            }, 800);
+        }
+    };
+
+    startButton.addEventListener('click', handleStartGame);
+    blocksContainer.addEventListener('click', handleBlockClick);
 })(5);
